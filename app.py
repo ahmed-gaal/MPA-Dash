f2aa1136d87769631a533836164a58e521d16769
ahmed14-cell <ahmedito14@gmail.com>
2021-01-31 20:01:43 +0300

    added basic authentication

--- app.py
+++ app.py
@@ -1,13 +1,24 @@
+
+import os
 import dash
 import dash_bootstrap_components as dbc
+import dash_auth as da
 
 # bootstrap theme
 # https://bootswatch.com/lux/
 external_stylesheets = [dbc.themes.LUX]
 
+valid_login = {
+    str(os.environ.get('USER')): str(os.environ.get('PASS'))
+}
+
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets,meta_tags=[
         {"name": "viewport", "content": "width=device-width, initial-scale=1"}
     ])
+
+auth = da.BasicAuth(
+    app, valid_login
+)
 server = app.server
 
 #if __name__ =='__main':
