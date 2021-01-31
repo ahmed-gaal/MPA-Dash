bf434324191e9e782f27a0ac49e35c38cd581ee5
ahmed14-cell <ahmedito14@gmail.com>
2021-01-31 18:41:17 +0300

    added extra graphs

--- explore.py
+++ explore.py
@@ -1,14 +1,19 @@
 import pandas as pd
 import cufflinks
 import plotly.express as px
+import dash_trich_components as dtc
 import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 from app import app
+from datetime import datetime
 
 df = pd.read_csv('new.csv')
+data = pd.read_csv('dataset.csv')
 gross = pd.read_csv('gross.csv')
+now = datetime.now()
+current_month = now.strftime('%B')
 
 available_indicators = df['Month'].unique()
 available_columns = df.columns
@@ -24,7 +29,7 @@ layout = html.Div([
                         options=[{
                             'label': i, 'value': i
                         } for i in available_columns],
-                        value='Total_Revenue'
+                        value='Total Revenue'
                     ),
                     dcc.RadioItems(
                         id='xaxis-type',
@@ -37,7 +42,7 @@ layout = html.Div([
                         options=[{
                             'label': i, 'value': i
                         } for i in available_indicators],
-                        value='Month'
+                        value=str(current_month)
                     ),
                     dcc.RadioItems(
                         id='yaxis-type',
@@ -52,7 +57,9 @@ layout = html.Div([
                         'showTips': True
                     }
                 )
-            ]), width=6
+            ], style = {
+                'font-variant': 'small-caps', 'font-weight': 'bold'
+            }), width=6
         ),
         dbc.Col(
             html.Div([
@@ -62,7 +69,7 @@ layout = html.Div([
                         options=[{
                             'label': i, 'value': i
                         } for i in available_columns],
-                        value='Albayrak_Share'
+                        value='Albayrak Share'
                     ),
                     dcc.RadioItems(
                         id='xaxis-type2',
@@ -76,7 +83,9 @@ layout = html.Div([
                         'displaylogo': False
                     }
                 )
-            ]), width=6
+            ], style = {
+                'font-variant': 'small-caps', 'font-weight': 'bold'
+            }), width=6
         )
     ], className='row'),
     html.Hr(),
@@ -103,7 +112,9 @@ layout = html.Div([
                     'displaylogo': False
                 }
             )
-            ]), width=6
+            ], style = {
+                'font-variant': 'small-caps', 'font-weight': 'bold'
+            }), width=6
         ),
         dbc.Col(
             html.Div([
@@ -112,8 +123,8 @@ layout = html.Div([
                         id='xaxis-column4',
                         options=[{
                             'label': i, 'value': i
-                        } for i in gross_columns],
-                        value='Net Revenue'
+                        } for i in available_indicators],
+                        value=str(current_month)
                     ),
                     dcc.RadioItems(
                         id='xaxis-type4',
@@ -127,7 +138,9 @@ layout = html.Div([
                     'displaylogo': False
                 }
             )
-            ]), width=6
+            ], style = {
+                'font-variant': 'small-caps', 'font-weight': 'bold'
+            }), width=6
         )
     ])
 ])
@@ -174,7 +187,7 @@ def update_graph_one(xaxis_column, xaxis_type):
         asFigure=True, kind='scatter', x='Month', mode='lines+markers',
         y=xaxis_column, xTitle='2018', yTitle='Amount in USD',
         colorscale='prgn', subplots=True, subplot_titles=True, theme='white',
-        gridcolor='white', interpolation='spline'
+        gridcolor='white', interpolation='spline', legend=True
     )
     fig.update_layout(margin={'l': 40, 'b': 40, 't': 40, 'r': 40})
     
@@ -186,11 +199,14 @@ def update_graph_one(xaxis_column, xaxis_type):
     Input('xaxis-column4', 'value'),
     Input('xaxis-type4', 'value'))
 def update_graph_two(xaxis_column, xaxis_type):
-    fig = gross.iplot(
-        asFigure=True, kind='pie', labels='Month', barmode='overlay',
-        values=xaxis_column, hole=0.4, pull=0.05, textposition='outside',
-        colorscale='rdylbu',linecolor='black', theme='white',
-        textinfo='label+percent', sort=False
+    dts = data[data['Month'] == xaxis_column]
+    ss = dts['Month'].unique()
+    colors = ['#7EAFED', '#6192BA', '#2D2F89', '#465DAB', '#1E1F2E']
+    fig = dts.iplot(
+        asFigure=True, kind='pie', labels='Element', barmode='overlay',
+        values='Amount in USD', hole=0.5, pull=0.02, textposition='inside',
+        colors=colors, linecolor='black', theme='white',
+        textinfo='percent', sort=False
     )
     fig.update_layout(margin={'l': 40, 'b': 40, 't': 40, 'r': 40})
     
